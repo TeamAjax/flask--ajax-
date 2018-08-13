70d62a9d891ff275aec18e468cb7e4464949060c
Unknown <jmspredondo@gmail.com>
2018-08-13 18:34:49 +0800

    Added routes, models, forms and other files for file structure

---
+++ app/routes.py
@@ -0,0 +1,57 @@
+from flask import render_template, url_for, flash, redirect, request, jsonify
+from flask_login import current_user, login_user, logout_user, login_required
+import json
+from werkzeug.security import generate_password_hash
+from werkzeug.urls import url_parse
+
+from app import app, db
+from app.models import User
+from forms import LoginForm
+
+
+@app.route('/')
+@app.route('/index')
+@login_required
+def index():
+
+    return render_template('index.html', title='Home Page')
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    # validate here
+    username = request.form['username']
+    email = request.form['email']
+    password = generate_password_hash(request.form['password'])
+    user = User(username=username, email=email, role='borrower', password_hash=password)
+    #db.session.add(user)
+    #db.session.commit()
+    return jsonify(user.__repr__())
+
+
+@app.route('/users/', methods=['GET'])
+def user_search():
